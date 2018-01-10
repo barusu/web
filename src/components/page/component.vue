9f6c79fd33c208e59a6f4fb118af43c272b255d0
shiro <sunny@yui.org.cn>
2018-01-10 11:33:15 +0800

    move menu atyle to app

--- src/components/page/component.vue
+++ src/components/page/component.vue
@@ -24,7 +24,16 @@
             <li class="oo-menu-item"> <router-link :to="{name: 'message'}">Message</router-link> </li>
           </ul>
         </p>
-        <p class="oo-menu-item"> <router-link :to="{name: 'ui'}">UI</router-link> </p>
+        <input type="checkbox" id="ck_form" class="oo-ck">
+        <p class="oo-menu-group">
+          <label for="ck_form" class="oo-menu-group-title">
+            <div>Data Entry</div>
+            <span class="oo-arrow"></span>
+          </label>
+          <ul class="oo-menu-item-list">
+            <li class="oo-menu-item"> <router-link :to="{name: 'message'}">Message</router-link> </li>
+          </ul>
+        </p>
       </div>
     </div>
     <router-view class="content"></router-view>
@@ -46,96 +55,6 @@
     }
   }
 </script>
-
-<!-- menu -->
-<style lang="scss">
-  .oo-menu {
-    margin-top: 1em;
-    text-align: left;
-    font-family: Helvetica, Arial, sans-serif;
-    font-size: .14rem;
-    label {
-      cursor: pointer;
-    }
-  }
-  .oo-menu-group-title,
-  .oo-menu-item,
-  .oo-menu-title {
-    height: .4rem;
-    line-height: .4rem;
-    margin: 0 0 .02rem;
-    user-select: none;
-  }
-  .oo-menu-title {
-    font-size: .18rem;
-    padding: 0 .16rem 0 .3rem;
-  }
-  .oo-menu-group-title {
-    position: relative;
-    display: block;
-    font-size: .14rem;
-    padding: 0 .16rem 0 3em;
-    color: #9c9c9c;
-  }
-  .oo-menu-item {
-    padding: 0 .16rem 0 .4rem;
-    overflow: hidden;
-    transition: height .34s ease-in-out, margin .34s linear;
-    > a {
-      color: #444;
-    }
-  }
-  .oo-menu-item-list {
-    > .oo-menu-item {
-      padding-left: .6rem;
-    }
-  }
-  .oo-arrow {
-    position: absolute;
-    top: 50%;
-    right: .16rem;
-    width: .1rem;
-    height: .1rem;
-    transform: translateY(-50%);
-    &::before,
-    &::after {
-      content: '';
-      position: absolute;
-      top: .04rem; left: .04rem;
-      vertical-align: baseline;
-      background: #666;
-      width: .06rem;
-      height: .02rem;
-      border-radius: 2px;
-      transition: all 0.34s cubic-bezier(0.645,0.045,0.355,1);
-      transform-origin:16.7% 50%;
-    }
-    &::before {
-      transform: rotate(45deg);
-    }
-    &::after {
-      transform: rotate(135deg);
-    }
-  }
-  .oo-ck {
-    position: absolute;
-    clip: rect(0,0,0,0);
-  }
-  .oo-ck:checked + .oo-menu-group {
-    .oo-arrow {
-      &::before {
-      transform: rotate(-45deg);
-      }
-      &::after {
-        transform: rotate(225deg);
-      }
-    }
-    .oo-menu-item {
-      height: 0;
-      margin-bottom: 0;
-    }
-  }
-</style>
 
 <style lang="scss">
   $borderColor: #ddd;
