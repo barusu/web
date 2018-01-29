e8aeb35dd818c86eb95399774e3d72f29e4a8bec
shiro <sunny@yui.org.cn>
2018-01-29 18:19:38 +0800

    upload progress

--- src/libs/ajax.js
+++ src/libs/ajax.js
@@ -6,6 +6,19 @@ const host = 'http://www.yui.org.cn/';
 $.config.timeout = 10000;
 $.config.baseURL = host;
 
+function onprogress(evt){
+  var loaded = evt.loaded;     //已经上传大小情况
+  var tot = evt.total;      //附件总大小
+  var per = Math.floor(100 * loaded / tot);  //已经上传的百分比
+  console.log(per);
+}
+
+$.engine = () => {
+  var xhr = new XMLHttpRequest();
+  xhr.upload.addEventListener("progress", onprogress, false);
+  return xhr;
+};
+
 function error(err) {
   if(err.status === 401) {
     window.location.href = '/login';
