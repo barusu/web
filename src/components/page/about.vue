abaa01d2203980701b286448d858485cc7f78bcb
shiro <sunny@yui.org.cn>
2018-02-01 19:37:16 +0800

    add Backlog page (about)

---
+++ src/components/page/about.vue
@@ -0,0 +1,32 @@
+<template>
+  <main class="about">
+    <o-list :list="backlog" class="border" type="diamond" status="forgive"></o-list>
+  </main>
+</template>
+
+<script>
+  import $ from '@/libs/ajax';
+
+  export default {
+    data() {
+      return {
+        backlog: []
+      };
+    },
+    mounted() {
+      $.get('resource/game/list', {type: 3}, data => {
+        if(Array.isArray(data)) {
+          this.backlog = data;
+        }
+      });
+    }
+  };
+</script>
+
+<style lang="scss">
+  .about {
+    padding: 1em 2em;
+    font-size: .16rem;
+    text-align: left;
+  }
+</style>
