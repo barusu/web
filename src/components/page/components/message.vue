3995c09c5df192b14658b7d27730183ce2573d95
shiro <sunny@yui.org.cn>
2018-02-06 14:16:18 +0800

    add message doc & log

--- src/components/page/components/message.vue
+++ src/components/page/components/message.vue
@@ -8,6 +8,10 @@
       border-radius: 4px;
       color: #fff;
     }
+    .list {
+      font-size: 14px;
+      font-family: monospace;
+    }
   }
 </style>
 
@@ -31,18 +35,60 @@
       <div class="item">
         <o-button effect="spread" type="success" @click="showSuccessMessage('This is a success message')">Success</o-button>
       </div>
+      <div class="item">
+        <o-button effect="spread" @click="showMessage('10 second real man', 10000)">10 second</o-button>
+      </div>
+      <div class="item">
+        <o-button effect="spread" type="info" @click="showIconMessage('This is a info message', 'bilibili')">Icon</o-button>
+      </div>
     </div>
+    <h3><span>API</span></h3>
+    <p>组件提供了一些方法来打开消息提示.</p>
+    <o-list :list="list" class="border list" type="diamond"></o-list>
+    <o-doc :list="doc"></o-doc>
+    <p>Option</p>
+    <o-doc :list="optionDoc"></o-doc>
+    <h3><span>Update</span></h3>
+    <o-log :log="log"></o-log>
   </div>
 </template>
 
 <script>
   export default {
     data() {
-      return {};
+      return {
+        list: [
+          'this.$msg(option, [duration]);',
+          'this.$msg.info(option, [duration]);',
+          'this.$msg.success(option, [duration]);',
+          'this.$msg.warning(option, [duration]);',
+          'this.$msg.error(option, [duration]);'
+        ],
+        doc: [
+          {property: 'option', description: '提示文字正文或配置信息(配置细节看下表)', type: 'String / Object', default: ''},
+          {property: 'duration', description: '自动关闭的延时, 单位毫秒', type: 'Integer', default: '3000'}
+        ],
+        optionDoc: [
+          {property: 'message', description: '提示文字正文', type: 'String', default: ''},
+          {property: 'type', description: '提示框类型: info/success/warning/error, 如未设置icon会附加对应图标', type: 'String', default: ''},
+          {property: 'closed', description: '关闭时触发的回调函数', type: 'Function', default: ''},
+          {property: 'duration', description: '自动关闭的延时, 单位毫秒', type: 'Integer', default: '3000'},
+          {property: 'icon', description: '附加图标, 值为o-svg支持的所有值', type: 'String', default: ''}
+        ],
+        log: [
+          {
+            date: '2017-02-05',
+            log: [
+              {text: 'info图标还没有', status: 'error'},
+              {text: '已有的三个图标不成套,需替换', status: 'error'}
+            ]
+          }
+        ]
+      };
     },
     methods: {
-      showMessage(msg) {
-        this.$msg(msg);
+      showMessage(msg, duration) {
+        this.$msg(msg, duration);
       },
       showInfoMessage(msg) {
         this.$msg.info(msg);
@@ -55,6 +101,12 @@
       },
       showErrorMessage(msg) {
         this.$msg.error(msg);
+      },
+      showIconMessage(msg, icon) {
+        this.$msg({
+          message: msg,
+          icon: icon
+        });
       }
     }
   }
