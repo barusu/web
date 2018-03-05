ed98561d5da0cf18ae32ff78affe48e51363928f
shiro <sunny@yui.org.cn>
2018-03-05 17:23:56 +0800

    暂存

--- src/components/page/components/color.vue
+++ src/components/page/components/color.vue
@@ -15,34 +15,91 @@
         background: transparent;
       }
     }
+    .preview {
+      > div {
+        float: left;
+        width: 25%;
+        padding: .5em 1em;
+        p {
+          margin: 0;
+          line-height: 2;
+          color: inherit;
+          &.white {
+            color: #fff;
+          }
+          &.black {
+            color: #000;
+          }
+        }
+      }
+      label {
+        margin-top: .5em;
+        max-width: 400px;
+        .label {
+          line-height: 1;
+        }
+        svg {
+          width: 14px;
+        }
+      }
+      p {
+        margin: 0;
+      }
+    }
   }
 </style>
 
 <template>
   <div class="color">
     <h2><span>Color</span> <span class="chinese">配色</span></h2>
-    <p>我喜欢的一些配色.</p>
-    <p class="tip">点击色块即可复制16进制颜色数值.</p>
+    <p>方便在没有设计稿时选择配色.</p>
+    <p>背景色与纯黑、五十度灰、纯白背景的效果预览.</p>
+    <!-- <p class="tip">点击色块即可复制16进制颜色数值.</p> -->
+    <div class="preview clearfix">
+      <div :style="{'background': color}">
+        <p class="white">花开生两面</p>
+        <p class="black">人生佛魔间</p>
+      </div>
+      <div :style="{'color': color, 'background': '#000'}">
+        <p>一花一世界</p>
+        <p>一叶一如来</p>
+      </div>
+      <div :style="{'color': color, 'background': 'hsl(0,0%,50%)'}">
+        <p>一砂一极乐</p>
+        <p>一笑一尘埃</p>
+      </div>
+      <div :style="{'color': color, 'background': '#fff'}">
+        <p>回忆如墓</p>
+        <p>淡薄如素</p>
+      </div>
+      <p>
+        <label class="input-wrapper base">
+          <span class="label"><o-svg type="copy"></o-svg></span>
+          <input type="text" readonly :value="color" ref="color">
+        </label>
+        <o-button class="">RGB</o-button>
+      </p>
+    </div>
     <h3><span>Base</span></h3>
     <div class="clearfix">
-      <div class="item" v-for="(c, index) in colors1" :style="{background: c}" @click="copy(index, 'colorEl1')"><input type="text" readonly :value="c" ref='colorEl1'></div>
+      <div class="item" v-for="(c, index) in colors1" :style="{background: c}" @click="copy(c, index, 'colorEl1')"><input type="text" readonly :value="c" ref='colorEl1'></div>
     </div>
     <div class="clearfix">
-      <div class="item" v-for="(c, index) in colors1" :style="{color: c, borderColor: c}" @click="copy(index, 'colorEl1')" v-html="c"></div>
+      <div class="item" v-for="(c, index) in colors1" :style="{color: c, borderColor: c}" @click="copy(c, index, 'colorEl1')" v-html="c"></div>
     </div>
     <h3><span>China</span></h3>
     <div class="clearfix">
-      <div class="item" v-for="(c, index) in colors2" :style="{background: c}" @click="copy(index, 'colorEl2')"><input type="text" readonly :value="c" ref='colorEl2'></div>
+      <div class="item" v-for="(c, index) in colors2" :style="{background: c}" @click="copy(c, index, 'colorEl2')"><input type="text" readonly :value="c" ref='colorEl2'></div>
     </div>
     <div class="clearfix">
-      <div class="item" v-for="(c, index) in colors2" :style="{color: c, borderColor: c}" @click="copy(index, 'colorEl2')" v-html="c"></div>
+      <div class="item" v-for="(c, index) in colors2" :style="{color: c, borderColor: c}" @click="copy(c, index, 'colorEl2')" v-html="c"></div>
     </div>
     <h3><span>Mac</span></h3>
     <div class="clearfix">
-      <div class="item" v-for="(c, index) in colors3" :style="{background: c}" @click="copy(index, 'colorEl3')"><input type="text" readonly :value="c" ref='colorEl3'></div>
+      <div class="item" v-for="(c, index) in colors3" :style="{background: c}" @click="copy(c, index, 'colorEl3')"><input type="text" readonly :value="c" ref='colorEl3'></div>
     </div>
     <div class="clearfix">
-      <div class="item" v-for="(c, index) in colors3" :style="{color: c, borderColor: c}" @click="copy(index, 'colorEl3')" v-html="c"></div>
+      <div class="item" v-for="(c, index) in colors3" :style="{color: c, borderColor: c}" @click="copy(c, index, 'colorEl3')" v-html="c"></div>
     </div>
   </div>
 </template>
@@ -51,16 +108,22 @@
   export default {
     data() {
       return {
+        color: '#fc605d',
         colors1: ['#409eff', '#67c23a', '#e6a23c', '#f56c6c', '#bbb', 'rgba(255,255,255,.5)', 'rgba(0,0,0,.5)'],
         colors2: ["#f0f4f3", "#d9eaf1", "#d5f0e7", "#a1e3c7", "#b36a63", "#d6b311", "#c96527", "#ff3102", "#9c2833", '#f25500', '#f00'],
         colors3: ['#fc605d', '#fdbc40', '#34c849']
       };
     },
     methods: {
-      copy(index, eln) {
+      copy(c, index, eln) {
         try {
-          this.$refs[eln][index].select();
-          document.execCommand('copy');
+          this.color = c;
+          this.$refs.color.select();
+          this.$nextTick(() => {
+            document.execCommand('copy');
+          });
+          // this.$refs[eln][index].select();
+          // document.execCommand('copy');
         }catch(e) {console.log(e); }
       }
     }
--- src/components/page/components/index.vue
+++ src/components/page/components/index.vue
@@ -11,14 +11,24 @@
     <h3><span>Css为主</span></h3>
     <p>除了必要的js,大多数的效果都是使用css来完成.</p>
     <p>因为大量效果是使用css3来完成,所以只兼容较新的浏览器,原则上对低版本浏览器或IE保障功能可用.</p>
+    <h3><span>Update</span></h3>
+    <o-log :log="log"></o-log>
   </div>
 </template>
 
 <script>
   export default {
     data() {
-      return {};
-    },
-    methods: {}
+      return {
+        log: [
+          {
+            date: '2018-03-05',
+            log: [
+              {status: 'error', text: '在button组件内发现一个filter的渲染问题,使用了filter的brightness后边框左右边缘部分遭到不明原因的裁剪,裁剪约0.5px,暂未确认在普通屏幕上的情况'}
+            ]
+          }
+        ]
+      };
+    }
   }
 </script>
