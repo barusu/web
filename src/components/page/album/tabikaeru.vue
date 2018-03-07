722d55ef3a41a7b3bd5ed5b3fdf323a9c83fe108
shiro <sunny@yui.org.cn>
2018-03-07 16:50:03 +0800

    。。。

--- src/components/page/album/tabikaeru.vue
+++ src/components/page/album/tabikaeru.vue
@@ -1,21 +1,4 @@
 <style lang="scss">
-  .color {
-    .item {
-      float: left;
-      width: 10em;
-      padding: 0 1em;
-      margin: 0 .05rem .1rem;
-      font-size: .14rem;
-      line-height: 2.5;
-      border-radius: 4px;
-      color: #fff;
-      border: 1px solid transparent;
-      input {
-        width: 100%;
-        background: transparent;
-      }
-    }
-  }
 </style>
 
 <template>
