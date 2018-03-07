d25848c62815b4296fabf9857a91a8719ddf40c1
shiro <sunny@yui.org.cn>
2018-03-07 16:37:22 +0800

    color页完工

--- src/components/page/components/color.vue
+++ src/components/page/components/color.vue
@@ -1,25 +1,12 @@
 <style lang="scss">
   .color {
-    .item {
-      float: left;
-      width: 9em;
-      padding: 0 1em;
-      margin: 0 .05rem .1rem;
-      font-size: .14rem;
-      line-height: 2.5;
-      border-radius: 4px;
-      color: #fff;
-      border: 1px solid transparent;
-      input {
-        width: 100%;
-        background: transparent;
-      }
-    }
     .preview {
       > div {
         float: left;
-        width: 25%;
+        width: 23%;
         padding: .5em 1em;
+        margin: 0 1% 5px;
+        box-shadow: 0 0 0 2px inset;
         p {
           margin: 0;
           line-height: 2;
@@ -39,6 +26,7 @@
         font-size: 12px;
         .label {
           line-height: 1;
+          cursor: pointer;
         }
         svg {
           width: 14px;
@@ -53,15 +41,70 @@
         margin: 0;
       }
     }
+    .tip {
+      margin: 12px 5px 0;
+    }
+    .colors {
+      position: relative;
+      margin-top: .5em;
+      padding: .5em 0;
+      border-radius: 4px;
+      background-size: 200%;
+      // background-image: radial-gradient(ellipse, rgba(252,96,93,0.3), rgba(253,188,64,0.3), rgba(52,200,73,0.3) , rgba(81,186,242,0.3), rgba(208,140,224,0.3));
+      // animation: moveBG 20s ease-in-out infinite;
+      overflow: hidden;
+      &::after {
+        content: '';
+        position: absolute;
+        top: 0; left: 0;
+        width: 2000%;
+        height: 1000%;
+        background-size: 25% 50%;
+        background-image: radial-gradient(ellipse,#fc605d, #fc605d, #fdbc40, #34c849, #d08ce0, #51baf2);
+        animation: slantMove 60s cubic-bezier(.5, .3, .5, .6) infinite;
+        opacity: .3;
+        z-index: -1;
+      }
+      > .group {
+        font-size: .2rem;
+        > .type {
+          float: left;
+          width: 5em;
+          text-align: center;
+          line-height: 2;
+          margin: .25em 0;
+        }
+        > .items {
+          overflow: hidden;
+        }
+      }
+      .item {
+        position: relative;
+        float: left;
+        width: 2em;
+        height: 2em;
+        border-radius: 50%;
+        margin: .25em;
+        cursor: pointer;
+        input {
+          position: absolute;
+          top: 40%; left: 40%;
+          background: transparent;
+          color: transparent;
+          z-index: -1;
+          font-size: 12px;
+          width: 1em;
+        }
+      }
+    }
   }
 </style>
 
 <template>
   <div class="color">
     <h2><span>Color</span> <span class="chinese">配色</span></h2>
-    <p>非组件,只是方便在没有设计稿时选择配色用.</p>
+    <p>非组件,只是方便在没有设计稿时选择配色用.(单击预览,双击复制)</p>
     <p>背景色与纯黑、五十度灰、纯白背景的效果预览.</p>
-    <!-- <p class="tip">点击色块即可复制16进制颜色数值.</p> -->
     <div class="preview clearfix">
       <div :style="{'background': Color}">
         <p class="white">花开生两面</p>
@@ -81,7 +124,7 @@
       </div>
       <p>
         <label class="input-wrapper base">
-          <span class="label"><o-svg type="copy"></o-svg></span>
+          <span class="label" @click="copy"><o-svg type="copy"></o-svg></span>
           <input type="text" readonly :value="Color" ref="color">
         </label>
         <o-button :type="type == 'HEX' ? 'info' : ''" @click="type = 'HEX'">HEX</o-button>
@@ -91,26 +134,32 @@
         <o-slider class="slider" :max="255" v-model="alpha" :disabled="!Alpha"></o-slider>
       </p>
     </div>
-    <h3><span>Base</span></h3>
-    <div class="clearfix">
-      <div class="item" v-for="(c, index) in colors1" :style="{background: c}" @click="copy(c, index, 'colorEl1')"><input type="text" readonly :value="c" ref='colorEl1'></div>
-    </div>
-    <div class="clearfix">
-      <div class="item" v-for="(c, index) in colors1" :style="{color: c, borderColor: c}" @click="copy(c, index, 'colorEl1')" v-html="c"></div>
-    </div>
-    <h3><span>China</span></h3>
-    <div class="clearfix">
-      <div class="item" v-for="(c, index) in colors2" :style="{background: c}" @click="copy(c, index, 'colorEl2')"><input type="text" readonly :value="c" ref='colorEl2'></div>
-    </div>
-    <div class="clearfix">
-      <div class="item" v-for="(c, index) in colors2" :style="{color: c, borderColor: c}" @click="copy(c, index, 'colorEl2')" v-html="c"></div>
-    </div>
-    <h3><span>Mac</span></h3>
-    <div class="clearfix">
-      <div class="item" v-for="(c, index) in colors3" :style="{background: c}" @click="copy(c, index, 'colorEl3')"><input type="text" readonly :value="c" ref='colorEl3'></div>
-    </div>
-    <div class="clearfix">
-      <div class="item" v-for="(c, index) in colors3" :style="{color: c, borderColor: c}" @click="copy(c, index, 'colorEl3')" v-html="c"></div>
+    <p class="tip">带透明度的八位十六进制颜色目前兼容性很差,Sass都还不认识.(2018.03)</p>
+    <div class="colors">
+      <div class="group">
+        <div class="type">Base</div>
+        <div class="items">
+          <div class="item" v-for="(c, index) in base" :style="{background: c}" @click="select(c)" @dblclick="copy(index, 'colorBase')"><input type="text" readonly :value="c" ref='colorBase'></div>
+        </div>
+      </div>
+      <div class="group">
+        <div class="type">Mac</div>
+        <div class="items">
+          <div class="item" v-for="(c, index) in mac" :style="{background: c}" @click="select(c)" @dblclick="copy(index, 'colorMac')"><input type="text" readonly :value="c" ref='colorMac'></div>
+        </div>
+      </div>
+      <div class="group">
+        <div class="type">Element</div>
+        <div class="items">
+          <div class="item" v-for="(c, index) in element" :style="{background: c}" @click="select(c)" @dblclick="copy(index, 'colorEle')"><input type="text" readonly :value="c" ref='colorEle'></div>
+        </div>
+      </div>
+      <div class="group">
+        <div class="type">China</div>
+        <div class="items">
+          <div class="item" v-for="(c, index) in china" :style="{background: c}" @click="select(c)" @dblclick="copy(index, 'colorChina')"><input type="text" readonly :value="c" ref='colorChina'></div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -190,9 +239,9 @@
       }
       if(alpha) {
         alpha = Math.round(parseInt(alpha, 16) * 100 / 255) / 100;
-        hex = `hsla(${h}, ${s}%, ${l}%, ${alpha})`;
+        hex = `hsla(${h},${s}%,${l}%,${alpha})`;
       }else {
-        hex = `hsl(${h}, ${s}%, ${l}%)`;
+        hex = `hsl(${h},${s}%,${l}%)`;
       }
     }
     return hex;
@@ -204,18 +253,25 @@
         type: 'HEX',
         Alpha: false,
         alpha: 127,
-        color: '#fc605d',
-        colors1: ['#409eff', '#67c23a', '#e6a23c', '#f56c6c', '#bbb', 'rgba(255,255,255,.5)', 'rgba(0,0,0,.5)'],
-        colors2: ["#f0f4f3", "#d9eaf1", "#d5f0e7", "#a1e3c7", "#b36a63", "#d6b311", "#c96527", "#ff3102", "#9c2833", '#f25500', '#f00'],
-        colors3: ['#fc605d', '#fdbc40', '#34c849']
+        color: '#fdbc40',
+        base: ['#000', '#333', '#666', '#999', '#ccc', '#fff'],
+        mac: ['#fc605d', '#fdbc40', '#34c849', '#d08ce0', '#71ca58', '#f7a650', '#f4cd56', '#51baf2', '#fd6461', '#a5a5a5', '#c3c3c3'],
+        element: ['#409eff', '#67c23a', '#e6a23c', '#f56c6c', '#303133', '#606266', '#909399', '#c0c4cc', '#dcdfe6', '#e4e7ed', '#ebeef5', '#f2f6fc'],
+        china: ["#f0f4f3", "#d9eaf1", "#d5f0e7", "#a1e3c7", "#b36a63", "#d6b311", "#c96527", "#ff3102", "#9c2833", '#f25500', '#f00']
       };
     },
     methods: {
-      copy(c, index, eln) {
+      select(c) {
+        this.color = c;
+        console.log('a');
+      },
+      copy(index, eln) {
         try {
-          this.color = c;
-          // this.$refs.color.select();
-          this.$refs[eln][index].select();
+          if(eln) {
+            this.$refs[eln][index].select();
+          }else {
+            this.$refs.color.select();
+          }
           document.execCommand('copy');
         }catch(e) {console.log(e); }
       }
