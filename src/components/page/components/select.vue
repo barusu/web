572fbc1577ce8ba101122c8d8098bf4faba52ea4
shiro <sunny@yui.org.cn>
2018-04-08 17:32:08 +0800

    add doc

--- src/components/page/components/select.vue
+++ src/components/page/components/select.vue
@@ -16,24 +16,19 @@
     <p>下拉选择器.</p>
     <div class="clearfix">
       <div class="item">
-        <o-input v-model="t" placeholder="基本"></o-input>
-      </div>
-      <div class="item">
         <o-select v-model="t" :data="data" placeholder="基本"></o-select>
-      </div>
-      <div class="item">
-        <o-input v-model="t" label="带文字"></o-input>
       </div>
       <div class="item">
         <o-select v-model="t" :data="data" placeholder="基本"  label="带文字"></o-select>
       </div>
       <div class="item">
-        <o-input v-model="t" type="single" label="单线条型"></o-input>
-      </div>
-      <div class="item">
-        <o-select v-model="t" type="single" :data="data" placeholder="基本"  label="单线条型"></o-select>
+        <o-select v-model="t" skin="single" :data="data" placeholder="基本"  label="单线条型"></o-select>
       </div>
     </div>
+    <h3><span>API</span></h3>
+    <o-doc :list="doc"></o-doc>
+    <h3><span>Events</span></h3>
+    <o-doc :list="eventsDoc" type="event"></o-doc>
   </div>
 </template>
 
@@ -50,7 +45,16 @@
           {value: 'sfa42', label: 'adssda'},
           {value: 'sfa32', label: 'afeda'}
         ],
-        t: ''
+        t: '',
+        doc: [
+          {property: 'label', description: '提示文本', type: 'String', default: ''},
+          {property: 'placeholder', description: 'placeholder', type: 'String', default: ''},
+          {property: 'readonly', description: '是否只读(属性存在未赋值同为true)', type: 'Boolean', default: 'false'},
+          {property: 'skin', description: '自定义皮肤,目前已有base,single两种,会作为class附加在最外层上', type: 'String', default: 'base'}
+        ],
+        eventsDoc: [
+          {eventName: 'v-model', description: '双向绑定(语法糖value&input可拆分使用)', callbackParamet: ''}
+        ]
       };
     },
     methods: {
