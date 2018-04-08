3ecee9bf23db9f84c9294d7ac5d31502186c197b
shiro <sunny@yui.org.cn>
2018-04-08 17:28:34 +0800

    adjust

--- src/components/base/checkbox.vue
+++ src/components/base/checkbox.vue
@@ -86,7 +86,7 @@
         top: 50%; left: .02rem;
         display: block;
         width: .1rem;
-        height: 0;
+        height: 2;
         border: 1px solid #fff;
         border-radius: 1px;
         transform: translate3D(0,-50%,0);
